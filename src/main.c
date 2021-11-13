e78b0b1d689b5705cf7aad7b4b5e1d74c5719ac9
Henrique Lourenco <henriquelourenco02@gmail.com>
2021-11-13 20:18:47 +0100

    ...

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
 #include <SDL2/SDL_scancode.h>
@@ -24,7 +23,7 @@
 
 #define mainGENERIC_PRIORITY (tskIDLE_PRIORITY)
 #define mainGENERIC_STACK_SIZE ((unsigned short)2560)
-#define STACK_SIZE 200
+#define STACK_SIZE 2000
 
 #define STATE_QUEUE_LENGTH 1
 
@@ -63,14 +62,6 @@
 #include "tracer.h"
 #endif
 
-static char *mq_one_name = "FreeRTOS_MQ_one_1";
-static char *mq_two_name = "FreeRTOS_MQ_two_1";
-aIO_handle_t mq_one = NULL;
-aIO_handle_t mq_two = NULL;
-aIO_handle_t udp_soc_one = NULL;
-aIO_handle_t udp_soc_two = NULL;
-aIO_handle_t tcp_soc = NULL;
-
 const unsigned char next_state_signal = NEXT_TASK;
 const unsigned char prev_state_signal = PREV_TASK;
 
@@ -82,10 +73,6 @@ static TaskHandle_t Task3 = NULL;
 static TaskHandle_t Task4 = NULL;
 static StaticTask_t Task2Buffer;
 static StackType_t xStack[ STACK_SIZE ];
-//static TaskHandle_t UDPDemoTask = NULL;
-//static TaskHandle_t TCPDemoTask = NULL;
-//static TaskHandle_t MQDemoTask = NULL;
-//static TaskHandle_t DemoSendTask = NULL;
 
 static QueueHandle_t StateQueue = NULL;
 static SemaphoreHandle_t DrawSignal = NULL;
@@ -366,39 +353,6 @@ void vDrawStaticItems(void)
     vDrawLogo();
 }
 
-void vDrawButtonText(void)
-{
-    static char str[100] = { 0 };
-
-    sprintf(str, "Axis 1: %5d | Axis 2: %5d", tumEventGetMouseX(),
-            tumEventGetMouseY());
-
-    checkDraw(tumDrawText(str, 10, DEFAULT_FONT_SIZE * 0.5, Black),
-              __FUNCTION__);
-
-    if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-        sprintf(str, "W: %d | S: %d | A: %d | D: %d",
-                buttons.buttons[KEYCODE(W)],
-                buttons.buttons[KEYCODE(S)],
-                buttons.buttons[KEYCODE(A)],
-                buttons.buttons[KEYCODE(D)]);
-        xSemaphoreGive(buttons.lock);
-        checkDraw(tumDrawText(str, 10, DEFAULT_FONT_SIZE * 2, Black),
-                  __FUNCTION__);
-    }
-
-    if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-        sprintf(str, "UP: %d | DOWN: %d | LEFT: %d | RIGHT: %d",
-                buttons.buttons[KEYCODE(UP)],
-                buttons.buttons[KEYCODE(DOWN)],
-                buttons.buttons[KEYCODE(LEFT)],
-                buttons.buttons[KEYCODE(RIGHT)]);
-        xSemaphoreGive(buttons.lock);
-        checkDraw(tumDrawText(str, 10, DEFAULT_FONT_SIZE * 3.5, Black),
-                  __FUNCTION__);
-    }
-}
-
 static int vCheckStateInput(void)
 {
     if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
@@ -415,117 +369,6 @@ static int vCheckStateInput(void)
     }
 
     return 0;
-}
-
-void UDPHandlerOne(size_t read_size, char *buffer, void *args)
-{
-    prints("UDP Recv in first handler: %s\n", buffer);
-}
-
-void UDPHandlerTwo(size_t read_size, char *buffer, void *args)
-{
-    prints("UDP Recv in second handler: %s\n", buffer);
-}
-
-void vUDPDemoTask(void *pvParameters)
-{
-    char *addr = NULL; // Loopback
-    in_port_t port = UDP_TEST_PORT_1;
-
-    udp_soc_one = aIOOpenUDPSocket(addr, port, UDP_BUFFER_SIZE,
-                                   UDPHandlerOne, NULL);
-
-    prints("UDP socket opened on port %d\n", port);
-    prints("Demo UDP Socket can be tested using\n");
-    prints("*** netcat -vv localhost %d -u ***\n", port);
-
-    port = UDP_TEST_PORT_2;
-
-    udp_soc_two = aIOOpenUDPSocket(addr, port, UDP_BUFFER_SIZE,
-                                   UDPHandlerTwo, NULL);
-
-    prints("UDP socket opened on port %d\n", port);
-    prints("Demo UDP Socket can be tested using\n");
-    prints("*** netcat -vv localhost %d -u ***\n", port);
-
-    while (1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-void MQHandlerOne(size_t read_size, char *buffer, void *args)
-{
-    prints("MQ Recv in first handler: %s\n", buffer);
-}
-
-void MQHanderTwo(size_t read_size, char *buffer, void *args)
-{
-    prints("MQ Recv in second handler: %s\n", buffer);
-}
-
-void vDemoSendTask(void *pvParameters)
-{
-    static char *test_str_1 = "UDP test 1";
-    static char *test_str_2 = "UDP test 2";
-    static char *test_str_3 = "TCP test";
-
-    while (1) {
-        prints("*****TICK******\n");
-        if (mq_one) {
-            aIOMessageQueuePut(mq_one_name, "Hello MQ one");
-        }
-        if (mq_two) {
-            aIOMessageQueuePut(mq_two_name, "Hello MQ two");
-        }
-
-        if (udp_soc_one)
-            aIOSocketPut(UDP, NULL, UDP_TEST_PORT_1, test_str_1,
-                         strlen(test_str_1));
-        if (udp_soc_two)
-            aIOSocketPut(UDP, NULL, UDP_TEST_PORT_2, test_str_2,
-                         strlen(test_str_2));
-        if (tcp_soc)
-            aIOSocketPut(TCP, NULL, TCP_TEST_PORT, test_str_3,
-                         strlen(test_str_3));
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-void vMQDemoTask(void *pvParameters)
-{
-    mq_one = aIOOpenMessageQueue(mq_one_name, MSG_QUEUE_MAX_MSG_COUNT,
-                                 MSG_QUEUE_BUFFER_SIZE, MQHandlerOne, NULL);
-    mq_two = aIOOpenMessageQueue(mq_two_name, MSG_QUEUE_MAX_MSG_COUNT,
-                                 MSG_QUEUE_BUFFER_SIZE, MQHanderTwo, NULL);
-
-    while (1)
-
-    {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-void TCPHandler(size_t read_size, char *buffer, void *args)
-{
-    prints("TCP Recv: %s\n", buffer);
-}
-
-void vTCPDemoTask(void *pvParameters)
-{
-    char *addr = NULL; // Loopback
-    in_port_t port = TCP_TEST_PORT;
-
-    tcp_soc =
-        aIOOpenTCPSocket(addr, port, TCP_BUFFER_SIZE, TCPHandler, NULL);
-
-    prints("TCP socket opened on port %d\n", port);
-    prints("Demo TCP socket can be tested using\n");
-    prints("*** netcat -vv localhost %d ***\n", port);
-
-    while (1) {
-        vTaskDelay(10);
-    }
 }
 
 void playBallSound(void *args)
@@ -561,9 +404,6 @@ void vTask1(void *pvParameters)
     ball_t *my_circle = createBall(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2, Red,
                                  RADIUS, 1000, &playBallSound, NULL);
 
-    static char str[100];
-    static int str_width = 0;
-
     while (1) {
         if (DrawSignal)
             if (xSemaphoreTake(DrawSignal, portMAX_DELAY) ==
@@ -577,8 +417,6 @@ void vTask1(void *pvParameters)
                 // Clear screen
                 checkDraw(tumDrawClear(White), __FUNCTION__);
                 vDrawStaticItems();
-                //vDrawCave(tumEventGetMouseLeft());
-                //vDrawButtonText();
                 
                 my_circle->x = SCREEN_WIDTH / 2;
                 my_circle->y = SCREEN_HEIGHT / 2;
@@ -643,26 +481,32 @@ void vTask3(void *pvParameters)
 
                 xGetButtonInput(); // Update global button data
 
-                xSemaphoreTake(ScreenLock, portMAX_DELAY);
-                // Clear screen
-                checkDraw(tumDrawClear(White), __FUNCTION__);
+                if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+                    if (buttons.buttons[KEYCODE(L)]) {
+                        xSemaphoreTake(ScreenLock, portMAX_DELAY);
+                        // Clear screen
+                        checkDraw(tumDrawClear(White), __FUNCTION__);
 
-                checkDraw(tumDrawCircle(200, 200, 30, Blue), __FUNCTION__);
+                        checkDraw(tumDrawCircle(200, 200, 30, Blue), __FUNCTION__);
 
-                vDrawStaticItems();
+                        vDrawStaticItems();
 
-                // Draw FPS in lower right corner
-                vDrawFPS();
+                        // Draw FPS in lower right corner
+                        vDrawFPS();
 
-                xSemaphoreGive(ScreenLock);
+                        xSemaphoreGive(ScreenLock);
+                    }
+                    xSemaphoreGive(buttons.lock);
+                }
+                
                 vCheckStateInput();
                 
-                vCheckButtonInput(KEYCODE(R), &n1, &button_flag);
+                /*vCheckButtonInput(KEYCODE(R), &n1, &button_flag);
                 
                 if (Task4) {
                     vTaskResume(Task4);
                 }
-                vTaskSuspend(Task3);
+                vTaskSuspend(Task3);*/
             }
     }
 }
@@ -676,30 +520,35 @@ void vTask4(void *pvParameters)
         if (DrawSignal)
             if (xSemaphoreTake(DrawSignal, portMAX_DELAY) ==
                 pdTRUE) {
-
+                tumFUtilPrintTaskStateList();
                 xGetButtonInput(); // Update global button data
 
-                xSemaphoreTake(ScreenLock, portMAX_DELAY);
-                // Clear screen
-                checkDraw(tumDrawClear(White), __FUNCTION__);
+                if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+                    if (buttons.buttons[KEYCODE(R)]) {
+                        xSemaphoreTake(ScreenLock, portMAX_DELAY);
+                        // Clear screen
+                        checkDraw(tumDrawClear(White), __FUNCTION__);
 
-                checkDraw(tumDrawCircle(200, 200, 30, Black), __FUNCTION__);
+                        checkDraw(tumDrawCircle(200, 200, 30, Black), __FUNCTION__);
 
-                vDrawStaticItems();
+                        vDrawStaticItems();
 
-                // Draw FPS in lower right corner
-                vDrawFPS();
+                        // Draw FPS in lower right corner
+                        vDrawFPS();
 
-                xSemaphoreGive(ScreenLock);
+                        xSemaphoreGive(ScreenLock);
+                    }
+                    xSemaphoreGive(buttons.lock);
+                }
 
                 vCheckStateInput();
 
-                vCheckButtonInput(KEYCODE(L), &n2, &button_flag);
+                /*vCheckButtonInput(KEYCODE(L), &n2, &button_flag);
 
                 if (Task3) {
                     vTaskResume(Task3);
                 }
-                vTaskSuspend(Task4);
+                vTaskSuspend(Task4);*/
             }
     }
 }
@@ -750,7 +599,8 @@ int main(int argc, char *argv[])
     logo_image = tumDrawLoadImage(LOGO_FILENAME);
 
     atexit(aIODeinit);
-//Load a second font for fun
+
+    //Load a second font for fun
     tumFontLoadFont(FPS_FONT, DEFAULT_FONT_SIZE);
 
     buttons.lock = xSemaphoreCreateMutex(); // Locking mechanism
@@ -790,67 +640,65 @@ int main(int argc, char *argv[])
         goto err_bufferswap;
     }
 
-    /** Tasks */
+    /** Demo Tasks */
     if (xTaskCreate(vTask1, "Task1", mainGENERIC_STACK_SIZE * 2,
-                    NULL, mainGENERIC_PRIORITY + 1, &Task1) != pdPASS) {
-        PRINT_TASK_ERROR("DemoTask1");
+                    NULL, mainGENERIC_PRIORITY + 5, &Task1) != pdPASS) {
+        PRINT_TASK_ERROR("Task1");
         goto err_task1;
     }
-    if (xTaskCreate(vTask2, "Task2", mainGENERIC_STACK_SIZE * 2,
-                    NULL, mainGENERIC_PRIORITY + 2, &Task2) != pdPASS) {
-        PRINT_TASK_ERROR("DemoTask2");
+
+    Task2 = xTaskCreateStatic(vTask2, "Task2", STACK_SIZE,
+                    NULL, mainGENERIC_PRIORITY + 4, xStack, &Task2Buffer);
+    if (Task2 == NULL) {
+        PRINT_TASK_ERROR("Task2");
         goto err_task2;
     }
 
     if (xTaskCreate(vTask3, "Task3", mainGENERIC_STACK_SIZE * 2,
                     NULL, mainGENERIC_PRIORITY + 3, &Task3) != pdPASS) {
-        PRINT_TASK_ERROR("DemoTask1");
+        PRINT_TASK_ERROR("Task3");
         goto err_task3;
     }
+
     if (xTaskCreate(vTask4, "Task4", mainGENERIC_STACK_SIZE * 2,
-                    NULL, mainGENERIC_PRIORITY + 4, &Task4) != pdPASS) {
-        PRINT_TASK_ERROR("DemoTask2");
+                    NULL, mainGENERIC_PRIORITY + 2, &Task4) != pdPASS) {
+        PRINT_TASK_ERROR("Task4");
         goto err_task4;
     }
-
-    vTaskSuspend(Task1);
-    vTaskSuspend(Task2);
-    vTaskSuspend(Task3);
-    vTaskSuspend(Task4);
-
-    tumFUtilPrintTaskStateList();
+    
+    //tumFUtilPrintTaskStateList();
 
     vTaskStartScheduler();
 
     return EXIT_SUCCESS;
 
-err_task4:
-    vTaskDelete(Task3);
-err_task3:
-    vTaskDelete(Task2);
-err_task2:
-    vTaskDelete(Task1);
-err_task1:
-    vTaskDelete(BufferSwap);
-err_bufferswap:
-    vTaskDelete(StateMachine);
-err_statemachine:
-    vQueueDelete(StateQueue);
-err_state_queue:
-    vSemaphoreDelete(ScreenLock);
-err_screen_lock:
-    vSemaphoreDelete(DrawSignal);
-err_draw_signal:
-    vSemaphoreDelete(buttons.lock);
-err_buttons_lock:
-    tumSoundExit();
-err_init_audio:
-    tumEventExit();
-err_init_events:
-    tumDrawExit();
-err_init_drawing:
-    safePrintExit();
-err_init_safe_print:
+    err_task4:
+        vTaskDelete(Task3);
+    err_task3:
+        vTaskDelete(Task2);
+    err_task2:
+        vTaskDelete(Task1);
+    err_task1:
+        vTaskDelete(BufferSwap);
+    err_bufferswap:
+        vTaskDelete(StateMachine);
+    err_statemachine:
+        vQueueDelete(StateQueue);
+    err_state_queue:
+        vSemaphoreDelete(ScreenLock);
+    err_screen_lock:
+        vSemaphoreDelete(DrawSignal);
+    err_draw_signal:
+        vSemaphoreDelete(buttons.lock);
+    err_buttons_lock:
+        tumSoundExit();
+    err_init_audio:
+        tumEventExit();
+    err_init_events:
+        tumDrawExit();
+    err_init_drawing:
+        safePrintExit();
+    err_init_safe_print:
     return EXIT_FAILURE;
 }
 
